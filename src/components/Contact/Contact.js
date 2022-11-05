1d8426e280a3002f73eca96ee47f232fd60ea495
Anigbo Christopher <octan311@gmail.com>
2022-11-05 22:20:39 +0100

    All Done

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import "./Contact.css";
+import map from "./../asset/map.jpg";
+
+const Contact = () => {
+  return (
+    <div className='form-wrap'> 
+    <form className='form-cover'>
+    <h1><b>Contact Me.</b></h1>
+    Hi there, contact me to ask me about anything you have in mind. <br></br><br></br><br></br>
+      <div className='name'>
+        <div className='first'>
+        <label for="name">First name </label> <br></br>
+          <input type="text" name="name" id="first_name" placeholder="Enter Your First name" /><br></br>
+        </div>
+
+        <div className='last'>
+          <label for="name">Last name </label> <br></br>
+            <input type="text" name="name" id="last_name" placeholder="Enter Your Last name" />
+        </div>
+      </div> <br></br>
+
+
+    <label className="mail" for="Email">Email</label> <br></br>
+    <input type="Email" name="" id="email" placeholder="yourname@email.com" /> <br></br>
+
+    <p className='reply'>Message</p>
+    <textarea name="" id="message" cols="20" rows="8" placeholder="Send me a message and I'll reply you as soon as possible."></textarea>
+    <br/><br></br>
+
+    <input type="checkbox" value="check" name="check" id="check"/> You Agree to providing your data to Christopher who may contact you. <br></br> <br></br>
+
+    <div className='send-cont'>
+        <button href='#' id='btn__submit' title="Click here to to Send Message."> Send Message</button>
+    </div>
+    
+      
+    </form> 
+
+    <div className="footer">
+        <div id="left">
+          <a href="https://hngi9.zuriboard.com/">Zuri.Internship</a>
+        </div>
+
+        <div id="center">
+          <a href="https://hngi9.zuriboard.com/">
+            HNG Internship 9 Frontend Task
+          </a>
+        </div>
+
+        <div id="right">
+          <img src={map} alt="any" id="logo_img" />
+          <a href="https://ingressive.org/">
+            INGRESSIVE <br></br> FOR GOOD
+          </a>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import "./Profile.css";
+import image1 from "./../asset/image1.jpeg";
+import map from "./../asset/map.jpg";
+import { FaGithub, FaSlack, FaShare } from "react-icons/fa";
+import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
+import { Link } from "react-router-dom";
+
+const Profile = () => {
+  return (
+   
+    <div id="profile_component">
+      <div id="profile_container">
+      <img src={image1} id="profile_img" alt="la casa" />
+      <h1 id="nomen">Christopher Anigbo</h1>
+      <div id="slack"> Chrstphr</div>
+      </div>
+
+      <div id="share">
+        <FaShare />
+      </div>
+      <div id="menu">
+        <HiOutlineDotsCircleHorizontal size={30} />
+      </div>
+
+      <div className="btn-cont">
+        <a href="https://twitter.com/Chrstphr_Wlkngs">Chrstphr_Wlkngs</a>
+        <a href="https://training.zuri.team/" id="btn__zuri">
+          Zuri Team
+        </a>
+        <a href="http://books.zuri.team" id="books">
+          Zuri Books{" "}
+        </a>
+        <a href="https://books.zuri.team/" id="python_book">
+          Python Books
+        </a>
+        <a href="https://background.zuri.team" id="pitch">
+          Background Check for Coders
+        </a>
+        <a href="https://books.zuri.team/design-rules" id="design_book">
+          Design Books{" "}
+        </a>
+       <Link to="/Contact"> Contact Me</Link>
+      </div>
+
+      {/* <div className="Logo">
+       
+      </div> */}
+      <div className='Logo'>
+        <li id="logo_img">
+          {" "}
+          <FaGithub size={30} />{" "}
+        </li>
+        <li id="logo_img">
+          {" "}
+          <FaSlack size={30} />{" "}
+        </li>
+    </div>
+      <div className="footer">
+        <div id="left">
+          <a href="https://hngi9.zuriboard.com/">Zuri.Internship</a>
+        </div>
+
+        <div id="center">
+          <a href="https://hngi9.zuriboard.com/">
+            HNG Internship 9 Frontend Task
+          </a>
+        </div>
+
+        <div id="right">
+          <img src={map} alt="any" id="logo_img" />
+          <a href="https://ingressive.org/">
+            INGRESSIVE <br></br> FOR GOOD
+          </a>
+        </div>
+      </div>
+    </div>
+    // <div>profile</div>
+  )
+}
+
+export default Profile
